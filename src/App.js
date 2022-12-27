5253453acf93b55715836ef41d48d0a4b1aa63bf
ojokne <ojokne@gmail.com>
2022-12-27 21:22:07 +0300

    Use protected routes
    This gives only authenticated users access to the protected routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 import Signup from "./components/Signup";
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
     <div className="App">
       <Routes>
         {/* <Route path="/" exact element={ } /> */}
-        <Route path="dashboard" element={<Dashboard />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,16 @@
+import { ACTIONS } from "../context/actions";
+import { useAuth } from "../context/context";
+
 function Dashboard() {
+  const { dispatch } = useAuth();
+  function handleLogout() {
+    dispatch({ type: ACTIONS.LOGOUT });
+  }
   return (
     <div>
       <h1>Dasboard</h1>
       <p>You are logged in</p>
+      <button onClick={() => handleLogout()}>Logout</button>
     </div>
   );
 }
